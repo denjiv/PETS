f7a1bcbb8cb4908b181fb6ed242146c3befee17f
Max Thompson <thompsonmaxanton@gmail.com>
2017-03-25 17:50:30 -0700

    Added Than's data and put all drive data in one folder

--- SpeedPrediction/SpeedPredictionML.py
+++ SpeedPrediction/SpeedPredictionML.py
@@ -5,25 +5,27 @@ import random as r
 import matplotlib.pyplot as plt
 import numpy as np
 
-def train_dataset(x_train, y_train):
+def train_dataset(speed_limits, waze_speeds, y_train):
 
 	y_train = y_train.reshape( -1, 1 )
 
-	ds = SupervisedDataSet(len(x_train), len(y_train))
-	for i in range(len(x_train)):
-		ds.appendLinked(x_train[i], y_train[i])
+	ds = SupervisedDataSet(2, 1)
+	for i in range(len(y_train)):
+		ds.addSample((speed_limits[i], waze_speeds[i]), (y_train[i],))
 
 	hidden_size = 100   # arbitrarily chosen
 
-	net = buildNetwork(len(x_train), hidden_size, len(y_train), bias = True )
+	net = buildNetwork(2, 2, 1, bias = True )
 	trainer = BackpropTrainer( net, ds )
 
 	print ("Training Model...")
 	print (trainer.trainUntilConvergence(verbose = True, maxEpochs = 5)) # gives double proportional to error
 	print ("Done Training.")
 
+	predicted_values = []
 
-	predicted_values = net.activate(x_train)
+	for i in range(len(y_train)):
+		predicted_values = net.activate((speed_limits[i], waze_speeds[i]))
 
 	return predicted_values
 
@@ -39,10 +41,10 @@ def make_fake_speed_data (aggression):
 		waze_speeds.append(65 + r.randint(-5, 5))
 		drive_speeds.append(65 + aggression + r.randint(-5, 5))
 
-	for i in range(500):
-		speed_limits.append(40)
-		waze_speeds.append(40 + r.randint(-5, 5))
-		drive_speeds.append(40 + aggression + r.randint(-5, 5))
+	# for i in range(500):
+	# 	speed_limits.append(40)
+	# 	waze_speeds.append(40 + r.randint(-5, 5))
+	# 	drive_speeds.append(40 + aggression + r.randint(-5, 5))
 
 	return (speed_limits, waze_speeds, drive_speeds)
 
@@ -52,11 +54,12 @@ def main():
 	waze_speeds = data[1]
 	drive_speeds = data[2]
 
+	speed_limits_a = np.asarray(speed_limits)
 	waze_speeds_a = np.asarray(waze_speeds)
 	drive_speeds_a = np.asarray(drive_speeds)
 
 
-	predicted_speeds = train_dataset(waze_speeds_a, drive_speeds_a)
+	predicted_speeds = train_dataset(speed_limits_a, waze_speeds_a, drive_speeds_a)
 
 	plt.subplot(1, 1, 1)
 	plt.plot(range(0, len(data[0])), predicted_speeds, color="r")
