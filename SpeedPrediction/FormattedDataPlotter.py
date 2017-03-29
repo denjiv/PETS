745afcd0b8c741b18f8588b4dfb8bd754e2122b7
thompsonmax <thompsonmaxanton@gmail.com>
2017-03-29 02:00:56 -0700

    graphs

--- SpeedPrediction/FormattedDataPlotter.py
+++ SpeedPrediction/FormattedDataPlotter.py
@@ -1,3 +1,8 @@
 
 
-plot_formatted_data(file_name, speed_limits="True")
+def plot_formatted_data(file_name, speed_limits="True"):
+
+	with open(file_name) as data_file:
+            in_data = json.load(data_file)
+
+    
--- SpeedPrediction/PlotSpeed.py
+++ SpeedPrediction/PlotSpeed.py
@@ -1,5 +1,6 @@
 import json
 import matplotlib.pyplot as plt
+from math import fabs
 
 def plot_speeds_v_time(data):
 
@@ -10,12 +11,12 @@ def plot_speeds_v_time(data):
 	predicted_speeds = []
 
 	for i in range(1, len(data) + 1):
-		if (data[str(i)]["type"].strip() == "WAZE"):
-			times.append(data[str(i)]["time(s)"])
-			speed_limits.append(data[str(i)]["speed_limit(mph)"])
-			drive_speeds.append(data[str(i)]["speed_act(mph)"])
-			waze_speeds.append(data[str(i)]["speed_waze(mph)"])
-			predicted_speeds.append(data[str(i)]["speed_predicted(mph)"])
+	#	if (data[str(i)]["type"].strip() == "WAZE"):
+			times.append(data[str(i)]["time"])
+			speed_limits.append(data[str(i)]["speed_limit"])
+			drive_speeds.append(data[str(i)]["drive_speed"])
+			waze_speeds.append(data[str(i)]["waze_speed"])
+			predicted_speeds.append(data[str(i)]["predicted_speed"])
 			
 	plt.subplot(1, 1, 1)
 	plt.plot(times, drive_speeds, color="r")
@@ -32,26 +33,58 @@ def plot_speeds_v_dist(data):
 	predicted_speeds = []
 
 	for i in range(1, len(data) + 1):
-		if (data[str(i)]["type"].strip() == "WAZE"):
-			distances.append(data[str(i)]["distance(mi)"])
-			speed_limits.append(data[str(i)]["speed_limit(mph)"])
-			drive_speeds.append(data[str(i)]["speed_act(mph)"])
-			waze_speeds.append(data[str(i)]["speed_waze(mph)"])
-			predicted_speeds.append(data[str(i)]["speed_predicted(mph)"])
+	#	if (data[str(i)]["type"].strip() == "WAZE"):
+			distances.append(data[str(i)]["distance"])
+			speed_limits.append(data[str(i)]["speed_limit"])
+			drive_speeds.append(data[str(i)]["drive_speed"])
+			waze_speeds.append(data[str(i)]["waze_speed"])
+			predicted_speeds.append(data[str(i)]["predicted_speed"])
 			
 	plt.subplot(1, 1, 1)
 	plt.plot(distances, drive_speeds, color="r")
 	plt.plot(distances, speed_limits, color="g")
 	plt.plot(distances, waze_speeds, color="b")
 	plt.plot(distances, predicted_speeds, color="m")
-	plt.show()
 
+	plt.ylabel('Speed (mph)')
+	plt.xlabel('Elapsed Distance (mi)')
+	plt.title("To Coeur D'Alene Speeds")
+
+def plot_actual_waze_error(data):
+	distances = []
+	errors = []
+
+	for i in range(1, len(data) + 1):
+	#	if (data[str(i)]["type"].strip() == "WAZE"):
+			distances.append(data[str(i)]["distance"])
+			errors.append(data[str(i)]["drive_speed"] - data[str(i)]["waze_speed"])
+
+	plt.subplot(1, 1, 1)
+	plt.plot(distances, errors)
+
+def plot_actual_v_waze(data):
+	waze = []
+	actual = []
+
+	for i in range(1, len(data) + 1):
+	#	if (data[str(i)]["type"].strip() == "WAZE"):
+			waze.append(data[str(i)]["waze_speed"])
+			actual.append(data[str(i)]["drive_speed"])
+
+	plt.subplot(1, 1, 1)
+	plt.scatter(waze, actual)
+	plt.ylabel('Actual Drive Speed (Training Data)')
+	plt.xlabel('Speed from Traffic Data')
+	plt.title("Scatter Plot of Actual v. Traffic Data speeds")
 
 def main():
-	with open("REI_data_w_predictions_total_nodes.json") as data_file:
+	with open("cda_data_w_predictions.json") as data_file:
 		data = json.load(data_file)
 	
 	#plot_speeds_v_time(data)
 	plot_speeds_v_dist(data)
+	#plot_actual_waze_error(data)
+	#plot_actual_v_waze(data)
+	plt.show()
 
 main()
